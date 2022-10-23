3cae094546c890ece3354005de50d0812ebb2ce6
Zhenya <manixjen@gmail.com>
2022-10-23 11:39:37 +0300

    upgrade read/write txt

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, SAXException {
-        XmlFileWorker file = new XmlFileWorker();
-        ArrayList list = file.read("in.xml");
-        file.write(list,"out.xml");
+        TxtFileWorker worker = new TxtFileWorker();
+        List<String> list =  worker.read("input.txt");
+        System.out.println(list.toString() + list.size());
+        worker.write((ArrayList) list,"output.txt");
 
     }
 }
--- src/TxtFileWorker.java
+++ src/TxtFileWorker.java
@@ -4,45 +4,51 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TxtFileWorker {
-    public static String readFileName = "input.txt";
-
     public static String writeFileName = "output.txt";
-    public static void read() throws IOException {
+    public static List<String> read(String fileName) throws IOException {
         BufferedReader br = null;
-
+        List<String> list = new ArrayList<>();
         try {
 
-            File file = new File(readFileName);
+            File file = new File(fileName);
 
             if(!file.exists()){
                 file.createNewFile();
             }
 
             String line;
-            br = new BufferedReader(new FileReader(readFileName));
+            br = new BufferedReader(new FileReader(fileName));
             while((line = br.readLine()) != null){
-                System.out.println(line);
+                list.add(line);
             }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             br.close();
+            return list;
+
         }
     }
 
-    public static void write(String line) throws IOException {
+    public static void write(List<String> list,String fileName) throws IOException {
         try{
-            File file = new File(writeFileName);
+            File file = new File(fileName);
 
             if(!file.exists()) {
                 file.createNewFile();
             }
 
             PrintWriter pr = new PrintWriter(file);
-            pr.println(line);
+            for(int i = 0;i < list.size(); i++){
+                String line = list.get(i);
+                pr.println(line);
+            }
             pr.close();
         }catch (IOException e) {
             throw new RuntimeException(e);
